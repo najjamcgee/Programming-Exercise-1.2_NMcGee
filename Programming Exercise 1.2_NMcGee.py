fa9775f0d295f2fb7fbc4d46bd1c2eb64e70087d
najjamcgee <136377802+najjamcgee@users.noreply.github.com>
2024-03-22 13:04:01 -0400

    Add files via upload

---
+++ Programming Exercise 1.2_NMcGee.py
@@ -0,0 +1,14 @@
+#Name: Najja McGee
+#Date: June 14th, 2023
+#Course: CIS 111
+#Section: 01
+#Program Name: Programming Exercise 1.2
+
+#Algorithm: 
+  #Step 1: Display my name
+  #Step 2: Display my address
+  #Step 3: Display my telephone number
+
+print("Najja McGee")
+print("9160 Saunders Drive SW")
+print("678-276-9528")
